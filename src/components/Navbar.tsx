3e342fb74d60f90918a207de535e0b787bbf56e9
Sahil Khan <118300201+Zer-0ne@users.noreply.github.com>
2024-10-10 18:16:20 +0000

    add animations when scrolling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,31 @@
 import { navbar } from '@/utils/constant'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Navbar = () => {
   const pathname = usePathname()
+  const [scrollY, setScrollY] = useState(0);
+
+  const handleScroll = () => {
+    setScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    // Set the initial scroll position
+    setScrollY(window.scrollY);
+
+    // Add the scroll event listener
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [scrollY]);
   return (
     <div className='w-[100%] sticky top-2 justify-center items-center flex flex-1 p-3 z-20'>
-      <div className={`min-w-[60%] h-[auto] flex flex-wrap justify-center items-center gap-6 p-2 rounded-full  ${pathname != '/domains' ? 'bg-[#C92842] text-white' : 'bg-transparent text-black'} `}>
+      <div className={`min-w-[60%] h-[auto] flex flex-wrap justify-center items-center gap-6 p-2 rounded-full transition-all ease-in-out delay-700 backdrop-blur-lg backdrop-saturate-200 ${scrollY<100?'bg-[#C92842]':'bg-[#c92843a6]'} text-white `}>
         { 
           navbar.map((item, index) => (
             <Link
